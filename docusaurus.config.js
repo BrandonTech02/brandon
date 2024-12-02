cee296ab5b6fc42c769aeb570f731ee084d348a7
BrandonTech02 <brandonteo0220@gmail.com>
2024-12-02 05:49:20 +0000

    2nd

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,12 +18,12 @@ const config = {
   url: 'https://BrandonTech02.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/my-demo/',
+  baseUrl: '/brandon/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'BrandonTech02', // Usually your GitHub org/user name.
-  projectName: 'my-demo', // Usually your repo name.
+  projectName: 'brandon', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
